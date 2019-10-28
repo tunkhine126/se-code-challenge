cf5cfe400b835b97c64e8ad8b9cb85a2503b0010
tunkhine126 <tunkhine126@gmail.com>
2019-10-28 13:17:52 -0400

    Fetched, Rendered, and some CSS

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 
+const URL = 'https://xkcd.now.sh/?comic=latest'
+
 class Home extends Component {
 
+  state = {
+    latestComic: []
+  }
+
+  componentDidMount() {
+    this.fetchLatest()
+  }
+
+  fetchLatest = () => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => this.setState({ latestComic: data }))
+  }
+
   render() {
+    { console.log(this.state.latestComic) }
+    const comic = this.state.latestComic
     return (
       <div>
-        This is the HOME component
+        <img className="latestImage" src={comic.img} title={comic.alt} alt={comic.title} />
       </div>
     )
   }
