42a7b8cbb30c8b1d1203e0b3782418e2dcf917a9
tunkhine126 <tunkhine126@gmail.com>
2019-10-28 12:35:59 -0400

    Routing working

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        This is the HOME component
+      </div>
+    )
+  }
+}
+
+export default Home;
